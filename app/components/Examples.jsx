0773483b78149062467e21c010aa8b13716d51f7
Jon Kemp <jonkemp12@gmail.com>
2017-05-18 18:02:27 +0100

    Added examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -13,12 +13,28 @@ var React = require('react');
 //   }
 // });
 
+// declare a link object to use:
+var {Link} = require('react-router');
+
 // Instead use stateless component
 var Examples = (props) => {
   return (
     <div>
-      <h2>Examples</h2>
-      <p>Welcome to examples page.</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples to try out:</p>
+
+      {/*Ordered list of links to home page with a city to query */}
+      <ol>
+        <li>
+          <Link to='/?city=Winchester,GB'>Winchester, GB</Link>
+        </li>
+        <li>
+          <Link to='/?city=London,GB'>London, GB</Link>
+        </li>
+        <li>
+          <Link to='/?city=Venice'>Venice</Link>
+        </li>
+      </ol>
     </div>
   )
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -23,8 +23,15 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2>Main container component</h2>
-      {props.children}
+      <div className="row">
+      {/* create a grid for all children
+        1/2 on medium screen, 1/3 on large, all centered */}
+        <div className="row">
+          <div className="columns medium-8 large-6 small-centered">
+            {props.children}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
