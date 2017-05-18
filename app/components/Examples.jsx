861cfb4feff93df5c34a4e1f569abfccfb83f43e
Jon Kemp <jonkemp12@gmail.com>
2017-05-18 13:59:11 +0100

    Update Examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -17,7 +17,7 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples child component!</h3>
+      <h2>Examples</h2>
       <p>Welcome to examples page.</p>
     </div>
   )
