c4afdd41649ab93ab4c9cfabd1250d48b0bb5c36
Jon Kemp <jonkemp12@gmail.com>
2017-06-06 11:26:44 +0100

    Fix ErrorModal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,5 +1,9 @@
 // For webpack - import the React libs:
 var React = require('react');
+// and ReactDOM:
+var ReactDOM = require('react-dom');
+// and ReactDOMServer
+var ReactDOMServer = require('react-dom/server');
 
 // Create a general error modal using Foundation Containers Reveal class
 // This can be used for various projects
@@ -23,16 +27,9 @@ var ErrorModal = React.createClass({
 
   // implement the life cycle method when it is run:
   componentDidMount: function () {
-    // Reveal the component using jQuery and its ID:
-    var modal = new Foundation.Reveal($('#error-modal'));
-    // Now open it up:
-    modal.open();
-  },
-
-  // Implement the render method:
-  render: function () {
     var {title, message} = this.props;
-    return (
+
+    var modalMarkup = (
       // Put meesages in and a button to close the modal
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
@@ -44,6 +41,29 @@ var ErrorModal = React.createClass({
         </p>
       </div>
     );
+
+    // jQuery variable to hold the HTML as a string:
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    // Now find the node:
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    // Reveal the component using jQuery and its ID:
+    var modal = new Foundation.Reveal($('#error-modal'));
+    // Now open it up:
+    modal.open();
+  },
+
+  // Implement the render method:
+  // To fix the problem with Foundation altering the DOM when this is called
+  // we need to move the code out to componentDidMount
+  // As Foundation messes with the DOM, we will render an empty doc.
+  render: function () {
+    return (
+      <div>
+
+      </div>
+    );
+
   }
 });
 
